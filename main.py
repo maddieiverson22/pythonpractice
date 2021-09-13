7867238415e8d405454899bf401950f5c227c334
Iverson Madeleine <825522@stma.k12.mn.us>
2021-09-13 15:10:46 -0500

    yo

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print("I love Elvis Presley!")
+
+#print first favorite movie
+print("Harry Potter")
+
+#print second favorite movie
+print("Nacho Libre")
+
+#print third favorite movie
+print("One Direction Documentary")
