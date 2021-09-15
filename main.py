7c2e82e6b67cac303dfae8b2532d91cca551432d
maddieiverson22 <825522@mystma.org>
2021-09-15 14:40:29 -0500

    added top three sports, types of pizza, and ice cream flavors

--- main.py
+++ main.py
@@ -9,4 +9,36 @@ def topFiveMovies():
     #print first movie
     print("One Direction Documentary")
 
+def topFiveSports():
+    #printfirstsport
+    print("Soccer")
+
+    #printsecondsport
+    print("Football")
+
+    #springthirdsport
+    print("Tennis")
+
+def topThreePizzas():
+    #printfirstpizza
+    print("hawaiian")
+
+    #printsecondpizza
+    print("supreme")
+
+    #printthirdpizza
+    print("bacon")
+
+def topThreeIceCreamFlavors():
+    #printfirsticecream
+    print("americone dream")
+
+    #printsecondicecream
+    print("chocolate chip cookiedough")
+
+    #printthirdicecream
+    print("cookies n' cream")
 topFiveMovies()
+topFiveSports()
+topThreePizzas()
+topThreeIceCreamFlavors()
