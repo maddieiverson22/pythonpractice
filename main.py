efd1a48a7567a5ef89a5525fb6c265671e7f6568
maddieiverson22 <825522@mystma.org>
2021-09-27 15:14:33 -0500

    dictionary

--- main.py
+++ main.py
@@ -1,75 +1,26 @@
-#define function
-def topFiveMovies():
-    #print first movie
-    print("Harry Potter")
-
-    #print first movie
-    print("Nacho Libre")
-
-    #print first movie
-    print("One Direction Documentary")
-
-def topFiveSports():
-    #printfirstsport
-    print("Soccer")
-
-    #printsecondsport
-    print("Football")
-
-    #springthirdsport
-    print("Tennis")
-
-def topThreePizzas():
-    #printfirstpizza
-    print("hawaiian")
-
-    #printsecondpizza
-    print("supreme")
-
-    #printthirdpizza
-    print("bacon")
-
-def topThreeIceCreamFlavors():
-    #printfirsticecream
-    print("americone dream")
-
-    #printsecondicecream
-    print("chocolate chip cookiedough")
-
-    #printthirdicecream
-    print("cookies n' cream")
-topFiveMovies()
-topFiveSports()
-topThreePizzas()
-topThreeIceCreamFlavors()
-print(11)
-print(22 + 14587)
-print(45 * 2)
-print(45**8)
-print (121 / 34)
-
-def myNameIs(x):
-    print(x + " is my name.")
-
-myNameIs("Maddie")
-myNameIs("Iverson")
-
-def plusFive(x):
-    print(x + 5)
-plusFive(45)
-
-def divideTwo(y):
-    print(y / 2)
-
-divideTwo(86)
-
-def addTwoNumbers(x, y):
-
-    print(x + y)
-
-addTwoNumbers(32,-10)
-
-def madLib(instrument, number, dayweek, letter, majorminor, numberb, numberc, adjective, adjectiveb, fraction, adjectivec, instrumentb, adjectived, noun, familymember, song, adjectivee, artist):
-    print("While I've stayed at home, I've practiced the " + instrument + " for " + number + " hours every " + dayweek + ". my favorite key signature to play and practice in is " + letter + majorminor + ". it has " + numberb + " flats and " + numberc + " sharps. I like this key signature because it is " + adjective + " and " + adjectiveb + " I've also gotten awesome at counting time signature. The time signaute I love is " + fraction + ", becuase it is " + adjectivec + " to count. One instrument I want to learn how to paly while I'm at home is " + instrumentb + ", because it is " + adjectived + " and sounds like a " + noun + ". My " + familymember + " likes it when I play " + song + ", and always gives me a round of applause after my performances! When I return to school, my tacher will be " + adjectivee + " of how great I am at playing my instrument. My teacher might even think I sound like " + artist + "!" )
-
-madLib("piano", "11", "monday", "z", "pizza", "d", "g", "amazing", "cool", "4/5", "gross", "guitar", "yummy", "dog", "grandma", "song", "purple", "elvis")
+def define(word):
+    word = word.lower()
+    if word == "zenith":
+        return "the highest point of something"
+    elif word == "zealot":
+        return "a fervent and even militant proponent of something"
+    elif word == "yearn":
+        return "desire strongly or persistently"
+    elif word == "yawner":
+        return "a person who yawns"
+    elif word == "xenophobia":
+        return "a fear of foreighners or strangers"
+    elif word == "x-axis":
+        return "the horizontal axis in a plane coordinate system"
+    elif word == "wonky":
+        return "turned or twisted toward one side"
+    elif word == "wanton":
+        return "a lewd or immoral person" 
+    elif word == "vermillion":
+        return "of a vivid red to reddish-orange color"
+    elif word == "vague":
+        return "lacking clarity or distinctness"
+    else:
+        return "that word is not in our dictionary"
+print(define("zenith"))
+print(define("pizza"))
